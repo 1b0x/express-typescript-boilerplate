9a758c16e3db813cda3e99a6287308693bba025c
Ibrahim Isuf <seoocs123@gmail.com>
2020-08-31 01:11:31 +0300

    Changed Data validation middleware (Added class type interface)

--- src/middlewares/DataValidation.ts
+++ src/middlewares/DataValidation.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+
 import { plainToClass } from "class-transformer";
+import { ClassType } from "class-transformer/ClassTransformer";
+
 import { validate } from "class-validator";
 
 import BadRequestException from "../exceptions/BadRequestException";
 
-export function DataValidation(type: any) {
+export function DataValidation(classType: ClassType<any>) {
     return async (req: Request, res: Response, next: NextFunction) => {
-        const a = plainToClass(type, req.body);
+        const a = plainToClass(classType, req.body);
 
         const errors = await validate(a);
         if (!errors.length) return next();
