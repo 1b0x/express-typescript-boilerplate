af1510ea96c061090297369307f7a2bd54224cb7
Ibrahim <seoocs123@gmail.com>
2020-08-30 23:09:02 +0300

    Add global input data validation; Create bad request exception/message

--- src/controllers/Authentication.ts
+++ src/controllers/Authentication.ts
@@ -1,15 +1,17 @@
-import { JsonController, Post, Body } from "routing-controllers";
+import { JsonController, Post, Body, UseBefore } from "routing-controllers";
 
 import { IUser } from "../interfaces/User";
 
 import User from "../database/entities/User";
 import AuthService from "../services/AuthService";
+import { DataValidation } from "../middlewares/DataValidation";
 
 @JsonController("/auth")
 export class UserController {
     constructor(private authService: AuthService) {}
 
     @Post("/login")
+    @UseBefore(DataValidation(User))
     login(@Body() user: any): any {
         return this.authService.login(user);
     }
