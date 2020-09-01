fe2e0f344d8efedc524d44681f90060e11d74751
Ibrahim <seoocs123@gmail.com>
2020-09-01 22:53:25 +0300

    Create user login validation

--- src/controllers/Authentication.ts
+++ src/controllers/Authentication.ts
@@ -3,6 +3,8 @@ import { JsonController, Post, Body, UseBefore } from "routing-controllers";
 import { IUser } from "../interfaces/User";
 
 import User from "../database/entities/User";
+import UserLogInDto from "../dto/UserLogInDto";
+
 import AuthService from "../services/AuthService";
 import { DataValidation } from "../middlewares/DataValidation";
 
@@ -11,7 +13,7 @@ export class UserController {
     constructor(private authService: AuthService) {}
 
     @Post("/login")
-    @UseBefore(DataValidation(User))
+    @UseBefore(DataValidation(UserLogInDto))
     login(@Body() user: any): any {
         return this.authService.login(user);
     }
